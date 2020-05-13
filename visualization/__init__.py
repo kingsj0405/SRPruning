029f4a966d22ed3a89813dee435478bc7ce727c4
kingsj0405 <kingsj0405@gmail.com>
2020-05-13 07:18:32 +0000

    Update documents

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -9,7 +9,13 @@ from visualization.cnn_layer_visualization import CNNLayerVisualization
 def _filter(net, save_dir, target_conv_index, filter_index):
     """
     summary:
-        it can be used in training
+        visualize filter by get activation of random input
+
+    parameters:
+        checkpoint_path: path to checkpoint
+        save_dir: path to save visualization
+        target_conv_index: conv index, start from 1
+        filter_index: filter index, start from 1
     """
     if target_conv_index != 'all':
         raise NotImplementedError
